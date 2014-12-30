214b6bcbab97daf8e233c8e078a93de2ad1c5d76
Mafuyu Kamono <kamomafu2181@yahoo.co.jp>
2014-12-30 23:09:37 +0900

    add refreshボタン（ネットワーク接続処理）

--- SwiftNews/ViewController.swift
+++ SwiftNews/ViewController.swift
@@ -9,6 +9,21 @@
 import UIKit
 
 class ViewController: UITableViewController {
+    let newsUrlString = "http://www.apple.com"
+    
+    // ニュースのアドレス
+    @IBAction func refresh(sender: AnyObject) {
+        // NSURLを作る
+        var url = NSURL(string: newsUrlString)!
+        
+        // データをダウンロードする
+        var task = NSURLSession.sharedSession().dataTaskWithURL(url, completionHandler: { data, response, error in
+            // ダウンロードサイズを表示する（テスト用）
+            println("done, length \(data.length)")
+        })
+        task.resume()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,17 +35,20 @@ class ViewController: UITableViewController {
     }
     
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
-        //テーブルの行数を返す
+        // テーブルの行数を返す
         return 5
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) ->UITableViewCell {
-        //セルを取得する
+        // セルを取得する
         var cell = tableView.dequeueReusableCellWithIdentifier("news") as UITableViewCell
         
-        //セルにテキストを設定する
+        // セルにテキストを設定する
         cell.textLabel?.text = "Swift News"
         
         return cell
-    }}
+    }
+
+
+}
 
