7730618b19e198621c194c92cdc38d1739608281
Mafuyu Kamono <kamomafu2181@yahoo.co.jp>
2015-01-12 18:15:10 +0900

    add JSONデータをパース

--- SwiftNews/ViewController.swift
+++ SwiftNews/ViewController.swift
@@ -9,20 +9,11 @@
 import UIKit
 
 class ViewController: UITableViewController {
-    let newsUrlString = "http://www.apple.com"
+    // エントリーの配列
+    var entries = NSArray()
     
-    // ニュースのアドレス
-    @IBAction func refresh(sender: AnyObject) {
-        // NSURLを作る
-        var url = NSURL(string: newsUrlString)!
-        
-        // データをダウンロードする
-        var task = NSURLSession.sharedSession().dataTaskWithURL(url, completionHandler: { data, response, error in
-            // ダウンロードサイズを表示する（テスト用）
-            println("done, length \(data.length)")
-        })
-        task.resume()
-    }
+    // ニュースサイトのアドレス
+    let newsUrlString = "http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&q=http://rss.dailynews.yahoo.co.jp/fc/rss.xml&num=8"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,21 +25,51 @@ class ViewController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
-        // テーブルの行数を返す
-        return 5
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // エントリーの数を返す
+        return entries.count
     }
     
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) ->UITableViewCell {
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         // セルを取得する
         var cell = tableView.dequeueReusableCellWithIdentifier("news") as UITableViewCell
         
-        // セルにテキストを設定する
-        cell.textLabel?.text = "Swift News"
+        // エントリーを取得する
+        var entry = entries[indexPath.row] as NSDictionary
+        
+        // セルにタイトルを設定する
+        cell.textLabel!.text = entry["title"] as? String
         
         return cell
     }
+    
+    // ニュースのアドレス
+    @IBAction func refresh(sender: AnyObject) {
+        // NSURLを作る
+        let url = NSURL(string: newsUrlString)!
 
-
+        // データをダウンロードする
+        var task = NSURLSession.sharedSession().dataTaskWithURL(url, completionHandler: { data, response, error in
+            
+            // JSONデータを辞書に変換する
+            var dict: NSDictionary! = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: nil) as? NSDictionary
+            
+            // /responseData/feed/entriesを取得する
+            if var responseData = dict?["responseData"] as? NSDictionary {
+                if var feed = responseData["feed"] as? NSDictionary {
+                    if var entries = feed["entries"] as? NSArray {
+                        // エントリーの配列を設定する
+                        self.entries = entries
+                    }
+                }
+            }
+            
+            // テーブルビューの更新をするため、マルチスレッドにスイッチする
+            dispatch_async(dispatch_get_main_queue(), {
+                // テーブルビューの更新をする
+                self.tableView.reloadData()
+            })
+        })
+        task.resume()
+    }
 }
-
